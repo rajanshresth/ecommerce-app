bfe6e03de314163a1ca09a509bea48042fdec5cd
rajanshresth <shrestharajan726@gmail.com>
2023-11-05 22:05:13 +0545

    Prisma setup with nextauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from '@/components/ui/button'
+import prisma from '@/server/db/client'
 
-export default function Home() {
+export default async function Home() {
+  const user = await prisma.user.findMany();
   return (
     <main className='flex flex-col space-y-2'>
       <h1>Rajan</h1>
-      <Button className='bg-blue-400 hover:bg-bl w-20'>Click me!!!</Button>
+      {user.map((user) => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      ))}
+      <Button className='bg-blue-400 hover:bg-blue-600 w-20'>Click me!!!</Button>
     </main>
   )
 }
