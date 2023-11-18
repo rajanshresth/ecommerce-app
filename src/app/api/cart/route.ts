4f45623dada6f087ef4a50dfc3445475a04c9474
rajanshresth <shrestharajan726@gmail.com>
2023-11-18 21:17:41 +0545

    type error fixed

--- src/app/api/cart/route.ts
+++ src/app/api/cart/route.ts
@@ -1,4 +1,5 @@
 import prisma from '@/server/db/client';
+import { OrderItem } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
@@ -9,7 +10,7 @@ export async function POST(request: NextRequest) {
         return null;
     }
 
-    const createdOrderItems = await prisma.orderItem.create({
+    const createdOrderItems: OrderItem = await prisma.orderItem.create({
         data: orderItem,
     });
     return NextResponse.json(createdOrderItems, { status: 201 });
