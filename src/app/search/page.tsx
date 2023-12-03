fd2f8456d23e650e69ddda7c1458e5146032f0ed
rajanshresth <shrestharajan726@gmail.com>
2023-12-03 20:26:49 +0545

    Search ui component

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,15 @@
 'use client';
+import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import { use, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import CartButton from '../cart/_component/add-to-cart';
 
-interface Product {
-    id: number;
+export interface Product {
+    id: string;
     name: string;
+    image: string;
+    price: number;
 }
 
 const SearchPage = () => {
@@ -26,12 +31,44 @@ const SearchPage = () => {
     }, [searchQuery]);
 
     return (
-        <div>
-            <div>
-                {products?.map((product) => (
-                    <div key={product.id}>{product.name}</div>
-                ))}
-            </div>
+        <div className='mt-8 grid grid-cols-1 gap-6 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            {products?.map((product) => (
+                <div
+                    className='group relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl'
+                    key={product.id}
+                >
+                    <Link
+                        // className='h-[400px] w-[400px] '
+                        href={`product/${product.id}`}
+                    >
+                        <Image
+                            alt='Product 1'
+                            className='object-cover'
+                            height='400'
+                            src={`${product?.image}`}
+                            style={{
+                                aspectRatio: '400/400',
+                                objectFit: 'cover',
+                            }}
+                            width='400'
+                        />
+                        <div className='flex-1 p-4'>
+                            <h3 className='font-semibold tracking-tight'>
+                                {product.name}
+                            </h3>
+                            <h4 className='font-semibold'>${product.price}</h4>
+                        </div>
+                    </Link>
+
+                    <CartButton
+                        product={{
+                            productId: product?.id,
+                            quantity: 1,
+                            price: product.price,
+                        }}
+                    />
+                </div>
+            ))}
         </div>
     );
 };
